c0a4052688bf123ff9810ff7f0f390f2ed6b314d
Tchinonso <lightersmercury@gmail.com>
2022-11-05 13:57:31 +0100

    node mailer example done

--- node.js
+++ node.js
@@ -7,6 +7,21 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user:
+		user: "your email@gmail.com",
+		pass: "your password"
 	}
-})
+});
+
+var mailOptions = {
+	from: "your email@gmail.com",
+	to: "myfriend@gmail.com",
+	subject: "sending email using nodejs",
+	text: "Hello friend!"
+};
+transporter.sendMail(mailOptions, function (error, info) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('email sent: ' + info.response);
+	}
+});
